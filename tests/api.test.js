20079fa74fb5672e1bee98c598a9401e90caad15
unknown <alfisalim.12@gmail.com>
2020-12-20 23:27:39 +0700

    Feat: added coverage to project

--- tests/api.test.js
+++ tests/api.test.js
@@ -8,23 +8,33 @@ const db = new sqlite3.Database(':memory:');
 const app = require('../src/app')(db);
 const buildSchemas = require('../src/schemas');
 
+const winston = require('winston');
+const logger = winston.createLogger({
+    transports: [
+        new winston.transports.Console()
+    ]
+});
+
 describe('API tests', () => {
     before((done) => {
         db.serialize((err) => { 
             if (err) {
                 return done(err);
+                logger.error("Something error");
             }
 
             buildSchemas(db);
 
             done();
+            logger.info("Successfully");
         });
     });
 
     describe('GET /', () => {
+
         it('should return ok', (done) => {
             request(app)
-                .get('/')
+                .get('/health')
                 .expect('Content-Type', /json/)
                 .expect(200, done);
         });
